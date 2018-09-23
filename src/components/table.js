17dc454746541c4c4760630faec8e289566486c9
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-09-22 20:40:56 -0400

    added temp component for showing api routes

--- src/components/table.js
+++ src/components/table.js
@@ -1,18 +1,52 @@
 import React, {Component} from 'react';
 import {Api} from "../network/api";
 import {TODAYS_REPORTS} from "../network/requests";
+import {getLinks} from "../redux/actions/link.actions";
+import {connect} from "react-redux";
+
 
 export class Table extends Component {
 
+    state = {
+        links: {}
+    };
+
     componentDidMount() {
-        Api.get(TODAYS_REPORTS).then(r => console.log(r))
-    }
+        Api.get(TODAYS_REPORTS).then(r => {
+            this.setState({links: r.links});
+        });
+        const {findLinks} = this.props;
+        findLinks();
+    };
 
     render() {
+        const {links} = this.state;
+        console.log(links);
         return (
-            <div>
-
-            </div>
+            <p>
+                {/*{JSON.stringify(links)};*/}
+            </p>
         );
     }
 }
+
+const mapStateToProps = state => {
+    const {links} = state;
+    return {
+        links
+    }
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+      findLinks: () => {
+          dispatch(getLinks());
+      }
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Table);
+
