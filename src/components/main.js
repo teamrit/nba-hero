6e23f6faf0669cd89da399c3cfb180223750838e
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-10-03 17:19:54 -0400

    added main component for routing

--- src/components/main.js
+++ src/components/main.js
@@ -4,6 +4,7 @@ import Table from "../components/table";
 import {Switch,Route} from "react-router";
 import {NavBar} from "./navbar";
 import Calender from "./calender";
+import {Teams} from "./teams";
 
 export class Main extends Component {
 
@@ -17,6 +18,8 @@ export class Main extends Component {
                 <Table />
                 <Switch>
                     <Route path="/calender" exact component={Calender} />
+                    <Route path="/teams" exact component={Teams} />
+                    <Route path="/teams/:id" exact component={Teams} />
                 </Switch>
             </div>
         );
