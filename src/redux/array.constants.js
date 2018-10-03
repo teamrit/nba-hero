973a647fefb132214aae1fca5f87e1e51912834f
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-10-03 17:34:07 -0400

    added constants to the app

---
+++ src/redux/array.constants.js
@@ -0,0 +1,8 @@
+export const H_TEAM="H_TEAM";
+export const V_TEAM="V_TEAM";
+export const DRAW="DRAW";
+export const PENDING="PENDING";
+
+export const POSSIBLE_OUTCOMES = [
+    H_TEAM,V_TEAM,DRAW,PENDING
+];
