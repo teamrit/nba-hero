a02bf66aeee2a891f8c8d678725f679d2c779b56
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-10-03 17:18:22 -0400

    added arena component

---
+++ src/components/arena.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class Arena extends Component {
+    render() {
+        const {arena} = this.props;
+        console.log(arena)
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
