9a2c974f99d8b60c27b1585f4e136cae41e4680a
Amrit Pal Singh <amritpalsingha4@gmail.com>
2018-10-04 11:22:31 -0400

    added actions for calender

--- src/redux/actions/calender.actions.js
+++ src/redux/actions/calender.actions.js
@@ -1,12 +1,21 @@
 import {Api} from "../../network/api";
 import {calenderLoadSuccess,calenderLoadFail} from "../dispatchers/calender.dispatcher";
+import {scoreBoardFail,scoreBoardSuccess} from '../dispatchers/scoreboard.dispatchers';
 
 export const getDates = (link) => dispatch =>
     Api.get(link)
     .then(response => {
-        console.log(response)
         return dispatch(calenderLoadSuccess(response));
     })
     .catch(error => {
         return dispatch(calenderLoadFail(error));
+    });
+
+export const getScoreBoard = (link) => dispatch =>
+    Api.get(link)
+    .then(response => {
+        return dispatch(scoreBoardSuccess(response))
+    })
+    .catch(error => {
+        return dispatch(scoreBoardFail(error))
     });
